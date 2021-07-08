89aac8536d7f580d066ebf4a0f74de9c2eee7920
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-07-08 13:25:00 +0600

    implode function done

--- 02.imolode_function/index.php
+++ 02.imolode_function/index.php
@@ -1,5 +1,8 @@
 <?php
-    
+    /* implode function always array elements convert to string */
+    $p_languages = array('HTML', 'CSS', 'PHP');
+    $p_tutorials = implode(', ',$p_languages);
+    var_dump($p_tutorials);
 
 
 ?>
