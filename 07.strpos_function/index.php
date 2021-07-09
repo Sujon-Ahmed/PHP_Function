c6df02e5852ba786c9cd48f1c774e51255ad27c1
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-07-09 20:57:52 +0600

    strpos function done

--- 07.strpos_function/index.php
+++ 07.strpos_function/index.php
@@ -1,4 +1,28 @@
 <?php
 
+    // substr position
+    //echo strpos("Hello_World!",'l');
+
+    // when ch isn't present in function
+    //$x = strpos("Hello_World!",'i');
+    //var_dump($x);
+
+    // when you know ch position then you assign the where count ch function
+    //echo strpos("Hello_World!",'l',4);
+
+    // check url
+
+    $x = strpos($_SERVER['REQUEST_URI'],'create_user');
+    if($x !== false){
+
+    echo '"create_user" substring found in URL';
+
+    }else{
+
+    echo '"create_user" substring not found in URL';
+
+    }
+
+    
 
 ?>
