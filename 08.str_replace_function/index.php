b25421cc079db370ff445cf99067d7d6d7261887
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-07-14 15:29:15 +0600

    str_replace function done

--- 08.str_replace_function/index.php
+++ 08.str_replace_function/index.php
@@ -1,4 +1,18 @@
 <?php
+    // echo str_replace('site','tutorials site','W3Schools is a nice site');
+
+    // str_replace with array
+   /*  $str = "Our Service available right now in Dhaka and Chittagong";
+    $old_city = array('Dhaka','Chittagong');
+    $current_city = array('Bogra','Rajshahi');
+    echo str_replace($old_city,$current_city,$str); */
+
+     // str_replace with count
+     $str = "Our Service available right now in Dhaka and Chittagong";
+     $old_city = array('Dhaka','Chittagong');
+     $current_city = array('Bogra','Rajshahi');
+     str_replace($old_city,$current_city,$str,$count);
+     echo $count;
 
 
 
