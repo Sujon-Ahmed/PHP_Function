e770c02473c732d3e5193acb73f693217c399afb
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-07-14 15:19:54 +0600

    str_replace function added

--- 07.strpos_function/index.php
+++ 07.strpos_function/index.php
@@ -3,16 +3,16 @@
     // substr position
     //echo strpos("Hello_World!",'l');
 
-    // when ch isn't present in function
-    //$x = strpos("Hello_World!",'i');
-    //var_dump($x);
+    //when ch isn't present in function
+    $x = strpos("Hello_World!",'i');
+    var_dump($x);
 
     // when you know ch position then you assign the where count ch function
     //echo strpos("Hello_World!",'l',4);
 
     // check url
 
-    $x = strpos($_SERVER['REQUEST_URI'],'create_user');
+  /*   $x = strpos($_SERVER['REQUEST_URI'],'create_user');
     if($x !== false){
 
     echo '"create_user" substring found in URL';
@@ -21,7 +21,7 @@
 
     echo '"create_user" substring not found in URL';
 
-    }
+    } */
 
     
 
