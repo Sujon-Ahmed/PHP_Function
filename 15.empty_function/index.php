5feab08866e2e522ca52cf9f87a41f59631f5941
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-07-20 20:33:01 +0600

    empty function added

---
+++ 15.empty_function/index.php
@@ -0,0 +1,13 @@
+<?php
+    // PHP empty() function the function can be used to verify the value of a variable.
+
+    // when variable value empty return true otherwise return true
+    /* $x ='';
+    $y = empty($x);
+    var_dump($y); */
+
+    // when array empty return true otherwise return true
+    $x = array('HTML','CSS');
+    $y = empty($x);
+    var_dump($y);
+?>
