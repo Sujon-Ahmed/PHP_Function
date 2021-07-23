ed4289c20a628d5cd10d49808769b910c25130dd
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-07-23 21:01:16 +0600

    wordwrap function added

---
+++ 23.wordwrap_function/index.php
@@ -0,0 +1,6 @@
+<?php
+    // wordwrap() function wrap a line
+
+    echo wordwrap("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae ipsam, architecto eveniet facere necessitatibus esse? Incidunt laborum veritatis veniam sed consectetur, animi reiciendis in repudiandae eligendi doloremque nesciunt, quaerat earum quasi dolores adipisci consequatur dolorem.",50,"<br>");
+
+?>
