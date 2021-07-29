724b7643748e5768750ca63ace225a1737fade04
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-07-29 21:11:22 +0600

    #example-2 added

--- 24.array_merge_function/index.php
+++ 24.array_merge_function/index.php
@@ -6,5 +6,13 @@
     echo '<pre>';
         print_r($final);
     echo '</pre>';
-    
+
+    // If there is a numeric key, whatever the value of that key is at the time of merge (value of the key) will start from zero and the current key will be deleted.
+
+    $language = array(5 => 'html', 6 => 'css');
+    $serverSiteLanguage = array('php','mysql');
+    $add = array_merge($language,$serverSiteLanguage);
+    echo '<pre>';
+        print_r($add);
+    echo '</pre>';
 ?>
