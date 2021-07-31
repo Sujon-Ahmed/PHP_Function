4bcde261ab13b4c4fa9dbe3d3238f78f86c3e6e1
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-07-31 21:23:58 +0600

    array_intersect function added

--- 25.array_intersect_function/index.php
+++ 25.array_intersect_function/index.php
@@ -1,8 +1,16 @@
 <?php
     // Example-1
-    $arr1 = array('id' => 15, 'name' => 'Sujon', 29);
+   /*  $arr1 = array('id' => 15, 'name' => 'Sujon', 29);
     $arr2 = array('customer_id' => 15, 'name' => 'Sujon', 57);
     $result = array_intersect($arr1,$arr2);
+    echo '<pre>';
+    print_r($result); */
+
+    // Example-2  return empty
+     $arr1 = array('id' => 15, 'name' => 'Sujon', 29);
+    $arr2 = array('customer_id' => 15, 'name' => 'Sujon', 57);
+    $arr3 = array('text' => 'something', 'name' => 'sujon', 75);
+    $result = array_intersect($arr1,$arr2,$arr3);
     echo '<pre>';
     print_r($result);
 
