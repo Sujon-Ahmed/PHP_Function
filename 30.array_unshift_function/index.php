0d660660d43b25f7e524a6904d2bd5717378119e
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-08-01 10:44:48 +0600

    array_unshift function added

---
+++ 30.array_unshift_function/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    $x = array('html', 'css', 'php');
+    $y = array_unshift($x, 'python', 'ruby', 'c++');
+    // var_dump($y);
+    echo '<pre>';
+    // print_r($x);
+    var_dump($x);
+
+?>
