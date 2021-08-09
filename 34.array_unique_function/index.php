03f300240720a1682b7bfab60be5b273dbc69942
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-08-09 21:33:14 +0600

    example with key

--- 34.array_unique_function/index.php
+++ 34.array_unique_function/index.php
@@ -1,10 +1,16 @@
 <?php
     // array_unique() always delete duplicate (value,element)
-    $element = array('html','css','html','php','java','php','javascript','perl','mysql');
-    $duplicate = array_unique($element);
+    // $element = array('html','css','html','php','java','php','javascript','perl','mysql');
+    // $duplicate = array_unique($element);
     
-    echo '<pre>';
+    // echo '<pre>';
     //var_dump($element);
-    print_r($duplicate);
+    // print_r($duplicate);
+
+    // example with key
+    // $x = array(5 => 'html', 2 => 'css','php', 'db' => 'mysql', 'database' => 'mysql', 1 => 'css');
+    // $y = array_unique($x);
+    // echo '<pre>';
+    // print_r($y);
 
 ?>
