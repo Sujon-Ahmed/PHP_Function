801f7c2cc51c871641f40419c4b0dde7f3aeb782
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-08-09 21:53:23 +0600

    example with multidimensional array

--- 35.array_reverse_function/index.php
+++ 35.array_reverse_function/index.php
@@ -8,9 +8,19 @@
     // print_r($y);
 
     // example-02
-    $x = array(5,'html','css', 6 => 'php', 'db' => 'mysql');
-    $y = array_reverse($x,true);
-    echo '<pre>';
-    print_r($y);
+    // $x = array(5,'html','css', 6 => 'php', 'db' => 'mysql');
+    // $y = array_reverse($x,true);
+    // echo '<pre>';
+    // print_r($y);
+
+    // Ex-03 with multidimensional array
+    $continents = array(
+        "asia" => array('bangladesh', 'india', 'pakistan'),
+        "europe" => array('england', 'france'),
+        "africa" => array('kenya', 'somalia', 'libya')
+    );
+    $result = array_reverse($continents);
+    echo "<pre>";
+    print_r($result);
 
 ?>
