fac8f1f537c848551c893652dcc5bf19c8f691c4
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-08-09 21:48:37 +0600

    Ex-02 add with true condition

--- 35.array_reverse_function/index.php
+++ 35.array_reverse_function/index.php
@@ -2,8 +2,14 @@
     // The array_reverse () function can be used to sort an array in reverse order.
 
     // example-01
+    // $x = array(5,'html','css', 6 => 'php', 'db' => 'mysql');
+    // $y = array_reverse($x);
+    // echo '<pre>';
+    // print_r($y);
+
+    // example-02
     $x = array(5,'html','css', 6 => 'php', 'db' => 'mysql');
-    $y = array_reverse($x);
+    $y = array_reverse($x,true);
     echo '<pre>';
     print_r($y);
 
