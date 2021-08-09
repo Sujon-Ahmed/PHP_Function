6c73f5250b4f878bb6c08f167b914e5c643f081c
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-08-09 22:35:48 +0600

    problem solve with array_search

--- 36.array_search_function/index.php
+++ 36.array_search_function/index.php
@@ -1,7 +1,32 @@
 <?php
     // array_search()
 
-    $x = array('html', 'css', 'php', 'mysql');
-    $y = array_search('php', $x);
-    echo $y;
+    // $x = array('html', 'css', 'php', 'mysql');
+    // $y = array_search('php', $x);
+    // echo $y;
+
+    // example 02 with 3rd parameter type
+    // $x = array('html', 'css', 'php', 'mysql', 55);
+    // $y = array_search('55', $x,true);
+    // var_dump($y);
+    // echo $y;
+
+    // with condition
+    // How to check if the array has value?
+    //  $x = array('html', 'css', 'php', 'mysql', 55);
+    //  if(array_search('html',$x)){
+    //      echo 'element fond.';
+    //  }else{
+    //      echo 'element not fond!';
+    //  }
+
+    // solution
+    $x = array('html', 'css', 'php', 'mysql', 55);
+     if(array_search('html',$x) !== false){
+         echo 'element fond.';
+     }else{
+         echo 'element not fond!';
+     }
+
+    
 ?>
