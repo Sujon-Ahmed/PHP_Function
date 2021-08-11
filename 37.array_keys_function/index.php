28ade9d748f728bc0eface8881d489f4ad3b612c
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-08-11 20:29:05 +0600

    with multidimensional array

--- 37.array_keys_function/index.php
+++ 37.array_keys_function/index.php
@@ -7,9 +7,20 @@
 
 
     // with second parameter
-    $x = array('html','css','php','mysql');
-    $y = array_keys($x,'mysql');
+    // $x = array('html','css','php','mysql');
+    // $y = array_keys($x,'mysql');
+    // echo '<pre>';
+    // print_r($y);
+
+
+    // with multidimensional array
+    $continents = array(
+        "asia" => array('bangladesh','india','pakistan'),
+        "europe" => array('england','france'),
+        "africa" => array('somalia','kenya','libya')
+    );
+    $result = array_keys($continents);
     echo '<pre>';
-    print_r($y);
+    print_r($result);
 
 ?>
