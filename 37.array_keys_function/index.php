e7b451e8346f65f180caa6c3ced325cc7d3303f4
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-08-11 20:18:39 +0600

    with second parameter

--- 37.array_keys_function/index.php
+++ 37.array_keys_function/index.php
@@ -1,7 +1,14 @@
 <?php
     // array_keys()
+    // $x = array('html','css','php','mysql');
+    // $y = array_keys($x);
+    // echo '<pre>';
+    // print_r($y);
+
+
+    // with second parameter
     $x = array('html','css','php','mysql');
-    $y = array_keys($x);
+    $y = array_keys($x,'mysql');
     echo '<pre>';
     print_r($y);
 
