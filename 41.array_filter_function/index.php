643c66c9b2a5dc3e289c9a79f242fb5dc10bfd88
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-08-15 10:44:42 +0600

    professional way

--- 41.array_filter_function/index.php
+++ 41.array_filter_function/index.php
@@ -13,15 +13,22 @@
 
     // example-2
 
-    $arr = [0 => 542, 1 => 'abc', 2 => 'xyz', 3 => 548, 4 => ''];
-    $new_arr = array_filter($arr, function($val){
-        if(is_int($val)){
-            return $val;
-        }
-    });
+    // $arr = [0 => 542, 1 => 'abc', 2 => 'xyz', 3 => 548, 4 => ''];
+    // $new_arr = array_filter($arr, function($val){
+    //     if(is_int($val)){
+    //         return $val;
+    //     }
+    // });
+    // echo '<pre>';
+    // print_r($new_arr);
+    // echo '</pre>';
+
+
+    // professional uses method
+    $arr = [0 => 454, 1 => 'abc', 2 => 'xyz', 3 => NULL, 4 => false, 5 => 768];
+    $new_arr = array_filter($arr);
     echo '<pre>';
     print_r($new_arr);
-    echo '</pre>';
 
 
 ?>
