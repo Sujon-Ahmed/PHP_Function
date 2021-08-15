3114284d69439087752b88c46d9a36c5420163de
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-08-15 09:08:08 +0600

    example without key

--- 39.array_flip_function/index.php
+++ 39.array_flip_function/index.php
@@ -1,7 +1,12 @@
 <?php
     // array_flip function always flip key to value and value to key
 
-    $x = array('markup' => 'html', 'stylesheet' => 'css', 'programming' => 'php', 'database' => 'mysql');
+    // $x = array('markup' => 'html', 'stylesheet' => 'css', 'programming' => 'php', 'database' => 'mysql');
+    // $y = array_flip($x);
+    // echo '<pre>';
+    // print_r($y);
+
+    $x = array('html', 'css', 'php', 'mysql');
     $y = array_flip($x);
     echo '<pre>';
     print_r($y);
