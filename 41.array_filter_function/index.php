e0b4777e6a15a678f1d94d9b1d4ca45d35af1edd
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-08-15 10:39:13 +0600

    Example-2 added

--- 41.array_filter_function/index.php
+++ 41.array_filter_function/index.php
@@ -1,14 +1,27 @@
 <?php
 
     // array_filter function
-    $arr = [0 => 542, 1 => 'abc', 2 => 'xyz', 3 => 374, 4 => ''];
-    function check_int($val){
+    // $arr = [0 => 542, 1 => 'abc', 2 => 'xyz', 3 => 374, 4 => ''];
+    // function check_int($val){
+    //     if(is_int($val)){
+    //         return $val;
+    //     }
+    // }
+    // echo '<pre>';
+    // print_r(array_filter($arr, 'check_int'));
+    // echo '</pre>';
+
+    // example-2
+
+    $arr = [0 => 542, 1 => 'abc', 2 => 'xyz', 3 => 548, 4 => ''];
+    $new_arr = array_filter($arr, function($val){
         if(is_int($val)){
             return $val;
         }
-    }
+    });
     echo '<pre>';
-    print_r(array_filter($arr, 'check_int'));
+    print_r($new_arr);
+    echo '</pre>';
 
 
 ?>
